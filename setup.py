859579b3762a470a03aa6ec84e13bba9a2768d64
domonik <schdruzzi@gmail.com>
2023-08-02 14:07:22 +0200

    added pypi setup

---
+++ setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages, Extension
+import versioneer
+
+
+NAME = "RDPMSpecIdentifier"
+DESCRIPTION = "Package for identification of RNA dependent Proteins from mass spec data "
+
+LONGDESC = DESCRIPTION #Todo: write Readme
+
+
+
+
+cmds = versioneer.get_cmdclass()
+setup(
+    name=NAME,
+    version=versioneer.get_version(),
+    cmdclass=cmds,
+    author="domonik",
+    author_email="dominik.rabsch@gmail.com",
+    packages=find_packages(),
+    package_dir={"RDPMSpecIdentifier": "./RDPMSpecIdentifier"},
+    license="LICENSE",
+    url="https://github.com/domonik/RDPMSpecIdentifier",
+    description=DESCRIPTION,
+    long_description=LONGDESC,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    package_data={
+        "RDPMSpecIdentifier.visualize": ["assets/*"],
+        "RDPMSpecIdentifier": ["tests/*.py", "tests/test_data/*"],
+    },
+    install_requires=[
+        "statsmodels",
+        "numpy",
+        "scipy",
+        "plotly",
+        "pandas",
+        "dash>=2.5",
+        "dash_bootstrap_components",
+        "scikit-bio",
+        "scikit-learn",
+        "dash_loading_spinners",
+        "kaleido"
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    scripts=[
+        "RDPMSpecIdentifier/executables.py",
+        "versioneer.py"
+    ],
+    entry_points={
+        "console_scripts": [
+            "RDPMSpecIdentifier = RDPMSpecIdentifier.executables:main"
+        ]
+    },
+)
