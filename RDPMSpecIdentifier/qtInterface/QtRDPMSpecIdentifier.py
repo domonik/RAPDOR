96614958ddc932b51565e43c568d50ac7408fef6
domonik <schdruzzi@gmail.com>
2023-08-09 14:04:04 +0200

    changed seperator to selectioN

--- RDPMSpecIdentifier/qtInterface/QtRDPMSpecIdentifier.py
+++ RDPMSpecIdentifier/qtInterface/QtRDPMSpecIdentifier.py
@@ -90,7 +90,7 @@ class DashRunner(QThread):
         self.text_output.setText(f"Server Running: {linkstr}")
         while self.dash_thread.is_alive():
             time.sleep(2)
-        self.text_output.setText(f"")
+        self.text_output.setText(f"Server Crashed")
 
 
     def quitserver(self):
@@ -99,6 +99,22 @@ class DashRunner(QThread):
         self.text_output.setText(f"")
 
 
+
+class ColorButton(QPushButton):
+    def __init__(self, *args, color):
+        super().__init__(*args)
+        self.color = QColor(color)
+        assert self.color.isValid()
+        self._set_style()
+
+    def _set_style(self):
+        style = f"background-color : {self.color.name()}"
+        self.setStyleSheet(style)
+
+    def setColor(self, color):
+        assert self.color.isValid()
+        self.color = color
+        self._set_style()
 
 
 
@@ -147,11 +163,22 @@ class RDPMSpecIdentifierGUI(QWidget):
         self.logbase.setText("None")
         layout.addWidget(self.logbase, 5, 1)
 
-        layout.addWidget(QLabel('seperator:'), 6, 0)
-        self.sep = QLineEdit()
-        self.sep.setText(",")
-        layout.addWidget(self.sep, 6, 1)
 
+        self.buttons = QHBoxLayout()
+        self.seperators = {"Comma": ",", "Tab": "\t", "Semicolon": ";", "Space": " "}
+        self.sep_boxes = []
+        layout.addWidget(QLabel("Seperator"), 6, 0)
+        for idx, (name, sep) in enumerate(self.seperators.items()):
+            box = QCheckBox(name)
+            if idx == 0:
+                box.setChecked(True)
+            self.buttons.addWidget(box)
+            self.sep_boxes.append(box)
+            box.toggled.connect(self.select_latest(idx))
+        self.buttons.setAlignment(Qt.AlignCenter)
+
+
+        layout.addLayout(self.buttons, 6, 1)
 
         layout.setAlignment(Qt.AlignTop)
         self.runServerBtn = QPushButton("Run Server")
@@ -161,7 +188,6 @@ class RDPMSpecIdentifierGUI(QWidget):
         self.killServerBtn = QPushButton("Kill Server")
         self.killServerBtn.clicked.connect(self.kill_server)
         self.killServerBtn.setDisabled(True)
-
         layout.addWidget(self.killServerBtn, 8, 0, 1, 2)
 
         svg = HeaderLine()
@@ -178,6 +204,29 @@ class RDPMSpecIdentifierGUI(QWidget):
         self.setWindowTitle("RDPMSpecIdentifier")
         self.setWindowIcon(QIcon(ICON))
 
+    def select_latest(self, idx):
+        def fct():
+            box = self.sep_boxes[idx]
+            any_checked = False
+            for idx2 in range(self.buttons.count()):
+                btn = self.sep_boxes[idx2]
+                if btn.isChecked() and btn.text() != box.text():
+                    btn.blockSignals(True)
+                    btn.setChecked(False)
+                    btn.blockSignals(False)
+                    any_checked = True
+            if not any_checked:
+                box.setChecked(True)
+
+        return fct
+
+    def _get_seperator(self):
+        for idx2 in range(self.buttons.count()):
+            btn = self.sep_boxes[idx2]
+            if btn.isChecked():
+                return self.seperators[btn.text()]
+        raise ValueError("Not all Buttons should be unchecked")
+
     def closeEvent(self, event):
         if self.thread is not None:
             self.kill_server()
@@ -190,7 +239,8 @@ class RDPMSpecIdentifierGUI(QWidget):
         design = self.file2.text.text()
         logbase = self.logbase.text()
         logbase = None if logbase == 'None' else int(logbase)
-        self.thread = DashRunner(intenstities, design, host, port, logbase, text_output=self.running, sep=self.sep.text())
+        sep = self._get_seperator()
+        self.thread = DashRunner(intenstities, design, host, port, logbase, text_output=self.running, sep=sep)
         self.thread.start()
         self.runServerBtn.setDisabled(True)
         self.killServerBtn.setEnabled(True)
